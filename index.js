0cf7819dfe67a24887557dab12b5566332b63678
Amit Yadav <amit.insan@gmail.com>
2024-08-12 21:49:19 +0530

    Revert "vercel route test"
    This reverts commit a824b6e61ef940e9937fe247b9f87ec809c51a7d.

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ const path = require('path');
 const app = express()
 const port = 3000
 
-app.get('/*', (req, res) => {
-    console.log(`Request URL: ${req.url}`); // Log request URL
-    // Your existing code...
-  });
-  
 app.use(expressLayouts)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'))
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.set('layout', 'partials/layout-horizontal');
@@ -18,11 +13,6 @@ app.set('layout', 'partials/layout-horizontal');
 app.get('/', (req, res) => {
     res.render('./pages/index.ejs')
 })
-
-app.get('/test', (req, res) => {
-    res.render('./pages/index.ejs')
-})
-
 
 app.get('/about-us.htm', (req, res) => {
     res.render('./pages/about-us.ejs')
@@ -36,34 +26,9 @@ app.get('/products.htm', (req, res) => {
     res.render('./pages/products.ejs')
 })
   
-app.get('/*', (req, res) => {
-    const requestedPath = req.path;
-  
-    // Check if the requested path ends with .htm
-    if (requestedPath.endsWith('.htm')) {
-      // Serve the .htm file if it exists in the public directory
-      res.sendFile(path.join(__dirname, 'public', requestedPath), err => {
-        if (err) {
-          res.status(404).send('File not found');
-        }
-      });
-    } else {
-      // Serve a default file or handle other routes
-      res.sendFile(path.join(__dirname, 'public', 'index.html'), err => {
-        if (err) {
-          res.status(404).send('File not found');
-        }
-      });
-    }
-  });
-
-  app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(500).send('Something broke!!!!')
-  })
 
 app.listen(port, () => {
   console.log(`App Running on port ${port}`)
 })
 
-module.exports = app;
+
