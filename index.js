218d9279f8845226d26f602ac8e31cf68a241bcf
Amit Yadav <amit.insan@gmail.com>
2024-08-12 17:40:32 +0530

    view path relative added

--- index.js
+++ index.js
@@ -10,23 +10,21 @@ app.set('view engine', 'ejs');
 app.set('layout', 'partials/layout-horizontal');
 
 app.get('/', (req, res) => {
-    res.render('pages/index.ejs')
+    res.render('./pages/index.ejs')
 })
 
 app.get('/about-us.htm', (req, res) => {
-    res.render('pages/about-us.ejs')
+    res.render('./pages/about-us.ejs')
 })
 
 app.get('/contact-us.htm', (req, res) => {
-    res.render('pages/contact-us.ejs')
+    res.render('./pages/contact-us.ejs')
 })
   
 app.get('/products.htm', (req, res) => {
-    res.render('pages/products.ejs')
+    res.render('./pages/products.ejs')
 })
   
-    
-
 
 app.listen(port, () => {
   console.log(`App Running on port ${port}`)
