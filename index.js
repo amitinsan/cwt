1ff3829116739ca4ad40bbffbcbbb1f886621348
Amit Yadav <amit.insan@gmail.com>
2024-08-12 17:43:03 +0530

    views path set

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
-
+const path = require('path');
 const app = express()
 const port = 3000
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.set('layout', 'partials/layout-horizontal');
 
