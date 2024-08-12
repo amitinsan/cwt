c0ce4e8e0a0faacb74bfb354bc795f4b970d7cf4
Amit Yadav <amit.insan@gmail.com>
2024-08-12 16:47:34 +0530

    express code check in

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express()
+const port = 3000
+
+app.use(expressLayouts)
+app.use(express.static('build'))
+app.set('view engine', 'ejs');
+app.set('layout', 'partials/layout-horizontal');
+
+app.get('/', (req, res) => {
+    res.render('pages/index.ejs')
+})
+
+app.get('/about-us.htm', (req, res) => {
+    res.render('pages/about-us.ejs')
+})
+
+app.get('/contact-us.htm', (req, res) => {
+    res.render('pages/contact-us.ejs')
+})
+  
+app.get('/products.htm', (req, res) => {
+    res.render('pages/products.ejs')
+})
+  
+    
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
